3fc86915d1f8799d4d214326610456137924d533
katopodis@mail.ru <katopodis@mail.ru>
2020-11-27 19:33:56 +0400

    TG-1 make fueture auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->enum('role', array('user','admin'))->default('user');
+            $table->string('first_name',191);
+            $table->string('last_name',191);
+            $table->string('email',191)->unique();
+            $table->integer('record');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
